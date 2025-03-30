e72a5d59b64da311f2051e89db132a1910fa3f34
Arne Johannessen <noreply@arne.johannessen.de>
2025-03-30 23:03:23 +0200

    Add hxtool.py script

---
+++ hxtool.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import hxtool, sys
+
+hxtool.main.main(sys.argv[1:])
+
+# This script simply executes hxtool, so as to avoid the need to install it properly.
+# May need hxtool.egg-info to work.
